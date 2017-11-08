0361de1515670908f98c7b2a7eb263434ca506bd
HeyItzGinger <tumtumjmh@gmail.com>
2017-11-07 19:24:57 -0800

    RobotInSpace.java SecretMessageBox.java TheRiddler.java XGeeks.java

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,13 +10,23 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+		 int score;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+String answer = JOptionPane.showInputDialog("What is at the end of a rainbow?");
+if (answer == "w"){
+	JOptionPane.showMessageDialog(null, "Correct!");
+	for (int i = 1; i < i; i++) {
+		
+	}
+ {
+		
+	}
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null, "Wrong! It's w!");
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
